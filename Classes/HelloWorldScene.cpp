a073a102e3b73089e2b8a79198944b4c29b9c16b
linyn0612 <liny>
2015-02-13 16:21:27 +0800

    create
    cocos2d-x3.3 自带物理引擎

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,98 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::createWithPhysics();
+    scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    visibleSize = Director::getInstance()->getVisibleSize();
+    
+    auto edge = PhysicsBody::createEdgeBox(visibleSize);
+    auto node = Node::create();
+    node->setPhysicsBody(edge);
+    node->setContentSize(visibleSize);
+    node->setPosition(visibleSize/2);
+    this->addChild(node);
+    
+    //touch listener
+    auto listener = EventListenerTouchOneByOne::create();
+    
+    listener->onTouchBegan = [this](Touch* t,Event *e){
+        
+        auto sprite = Sprite::create("iphone/CloseSelected.png");
+        auto body = PhysicsBody::createCircle(sprite->getContentSize().width);
+        body->setContactTestBitmask(count++);
+        sprite->setPhysicsBody(body);
+        sprite->setPosition(t->getLocation());
+
+        this->addChild(sprite);
+        
+        return false;
+    };
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+    
+    //contact listener
+    auto contactListener = EventListenerPhysicsContact::create();
+    contactListener->onContactBegin = [this](PhysicsContact & contact){
+        
+        auto a = (Sprite*)contact.getShapeA()->getBody()->getNode();
+        auto b = (Sprite*)contact.getShapeB()->getBody()->getNode();
+        
+        a->removeFromParent();
+        b->removeFromParent();
+        
+        return true;
+    };
+    
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(contactListener, this);
+    
+    //exit
+    auto rootNode = CSLoader::createNode("MainScene.csb");
+
+    addChild(rootNode);
+
+    auto closeItem = static_cast<ui::Button*>(rootNode->getChildByName("Button_1"));
+    closeItem->addTouchEventListener(CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+
+    return true;
+}
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
